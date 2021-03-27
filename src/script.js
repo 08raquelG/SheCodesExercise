d81710cf2b610a981f26983e5e90fedfea0b8f91
08raquelG <08raquelgouveia@gmail.com>
2021-03-27 11:08:29 +0000

    Insert project

---
+++ src/script.js
@@ -0,0 +1,55 @@
+let now = new Date();
+function formatDate(date) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  let currentDay = days[date.getDay()];
+  let currentHour = date.getHours();
+  let currentMinutes = date.getMinutes();
+
+  let formattedDay = `${currentDay}`;
+  let formattedHours = `${currentHour}:${currentMinutes}`;
+
+  let dateDay = document.querySelector(".day");
+  dateDay.innerHTML = formattedDay;
+
+  let dateHour = document.querySelector(".hours");
+  dateHour.innerHTML = formattedHours;
+}
+
+formatDate(now);
+
+let input = document.querySelector("#insert-place");
+
+function searchUp(event) {
+  event.preventDefault();
+
+  console.log(input.value);
+
+  let typedPlace = input.value;
+  let changePlace = document.querySelector("h1");
+  changePlace.innerHTML = typedPlace;
+
+  let apiKey = "804b257270bd9a0d0e4b7cd0e1785455";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${typedPlace}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchUp);
+
+function showTemperature(response) {
+  console.log(response);
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperatureNumber");
+  let description = document.querySelector("#weather-description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = `${temperature}`;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  wind.innerHTML = `Wind: ${response.data.wind.speed}km/h`;
+
+  console.log(response);
+}
+
